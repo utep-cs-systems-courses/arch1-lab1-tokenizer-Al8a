6e6e7d7034a7728afc75a6b4f3c194bd4dd63cf9
Alan Ochoa <aeochoa2@miners.utep.edu>
2022-02-17 02:13:56 -0700

    Replaced ui menu.

--- src/uimain.c
+++ src/uimain.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include "history.h"
@@ -6,49 +5,72 @@
 #define BUFFER_SIZE 100
 
 
+// if a '!' is read at user_input[0]
+static void process_input(char *user_input, List *history, int history_length){
+
+  // display history
+  if(user_input[1] == ' ' || user_input[1] == '\0' || user_input[1] == 'h' || user_input[0] == '!'){
+    printf("\t---- HISTORY ----\n");
+    print_history(history);
+    printf("\t-----------------\n");
+  }
+
+  // quit command
+  else if(user_input[1] == 'q'){
+    free_history(history);
+    exit(EXIT_SUCCESS);
+  }
+  
+  else{
+    int asked_index = atoi(user_input + 1); 
+
+    if(asked_index > 0){
+      char *str = get_history(history, history_length - asked_index + 1);
+
+      if(!str){
+	puts("---- NO HISTORY FOUND AT GIVEN INDEX ----");
+	return;
+      }
+      
+      char **tokens = tokenize(str);
+      print_tokens(tokens);
+      free_tokens(tokens);
+    }
+    else{
+      puts("---- INVALID INDEX ----");
+      return;
+    }
+  }
+  
+  // EXIT_SUCCESS == exit(0)
+  // EXIT_FAILURE == exit(1)
+}
+
 
 int main()
 {
   char user_input[BUFFER_SIZE];
-  int choice;
-
-  puts("\nWelcome to Tokenizer!\n");
-  while(1){
-    fputs("Please select an operation to run:\n\t1. Record and store a new string.\n\t2. Print a past entry.\n\t3 .Display all previous entries.\n\t4. Quit Tokenizer\n", stdout);
-    printf("> ");
-    fflush(stdout);
-
-    while ((choice = getchar()) == '\n');
-    if(choice == EOF)
-      goto done;
-    
-    switch(choice){
-    case '1':
-      puts("Please enter the new message to be recorded!\n ");
-      printf("> ");
-      fgets(user_input, BUFFER_SIZE, stdin);
-      break;
-    case '2':
-      puts("Please input a past entry index number.\n");
-      break;
-    case '3':
-      puts("Displaying entry history");
-      break;
-    case '4':
-      puts("Now exiting tokenizer");
-      goto done;
-    case '\n':
-      break;
-    default:
-      printf("Unrecongized option %c, please try again!\n", choice);
-    };
-
-  }  
- done:
-  exit(EXIT_SUCCESS);  // exit(0)
-}
-
-     
-    
-   
+  List *history = init_history();
+  char **tokens;
+  int history_length = 0;
   
+  puts("\nWelcome to Tokenizer!\n");
+  puts("Available operations to run:\n\t1. Type and store a new string.\n\t2. '!<num>' To print a past entry.\n\t3. '!h'|'!' Display all previous entries.\n\t4. '!q' Quit Tokenizer\n");
+  
+  while(1){
+  
+    printf("> ");
+    fgets(user_input, BUFFER_SIZE, stdin);
+
+    // check for '!' commands
+    if(user_input[0] == '!'){
+      process_input(user_input, history, history_length);
+    }
+   
+    else{
+      puts(user_input);
+      add_history(history, user_input);
+      history_length++;
+    }  
+  }
+}
